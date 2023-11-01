75b015db8e00ce596c5502cbdd619b27fc3de7b7
Kambar <kidkambar@gmail.com>
2023-11-02 01:56:28 +0500

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+  {
+    path: ':id',
+    component: PokemonDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
